3378c893fdcb7b8bf4e11c5a14411157b91ffa28
pragya-commits <109534314+pragya-commits@users.noreply.github.com>
2024-06-26 13:30:16 +0000

    completed datatypes and conversion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+console.log("pragya") //code readability should be high
+
+let name="pragya"// this is string dataType
+let age=24 //this is number
+let isLoggedIn=false // this is boolean dataType
+
+//number=>2 to power 53
+//string = >""
+//boolean =>true/false
+//null => standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof undefined);
